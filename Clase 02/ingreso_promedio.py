90cd286cf438a6eb0e64ce0089f97bb80a6b0aa8
BichoReactorDev <bichoreactor@gmail.com>
2024-10-01 21:05:22 -0300

    Commit inicial

---
+++ Clase 02/ingreso_promedio.py
@@ -0,0 +1,10 @@
+ingreso_enero   = 35632
+ingreso_febrero = 3223432
+ingreso_marzo   = 434534
+ingreso_abril   = 3424323
+ingreso_mayo    = 32432432
+ingreso_junio   = 1231255
+
+ingreso_promedio = (ingreso_enero + ingreso_febrero + ingreso_marzo + ingreso_abril + ingreso_mayo + ingreso_junio) / 6
+
+print("El ingreso promedio del primer semestre es", ingreso_promedio)
