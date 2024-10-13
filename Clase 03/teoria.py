53b93fe15ff74bceb192f17c774605f35b74eca1
Bicho Reactor <musica.bichoreactor@gmail.com>
2024-10-13 17:04:42 -0300

    Clase 3.

---
+++ Clase 03/teoria.py
@@ -0,0 +1,23 @@
+# Comentarios de una línea 
+
+"""
+Comentarios de más de una línea
+Algo así como este
+O algún otro
+"""
+
+cadena = "Hola Python"
+entero = 47
+flotante = 9.12
+
+print(type(cadena))
+print(type(entero))
+print(type(flotante))
+
+# Entrar texto por pantalla
+un_numero   = int(input("Ingresá un número: "))
+otro_numero = int(input("Ingresá otro número: "))
+
+suma = un_numero + otro_numero
+
+print(f"La suma de los números ingresados es {suma}") 
