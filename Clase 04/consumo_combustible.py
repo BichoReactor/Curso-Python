4aba5c29b2e28167fa73d62cae27b5e71c710475
Bicho Reactor <bichoreactor@gmail.com>
2024-10-15 13:47:58 -0300

    Clase 4 - Ejercicio 2 (no terminado)

---
+++ Clase 04/consumo_combustible.py
@@ -0,0 +1,10 @@
+"""
+Ejercicio 2: Consumo de Combustible:
+
+Realizar una aplicación en Python que a partir de la cantidad de litros de combustible que consume un coche por cada 100 km de recorrido, 
+calcule el costo de cada litro de combustible y la longitud del viaje realizado (en kilómetros).
+Mostrar un detalle de los litros consumidos y el dinero gastado.
+"""
+
+print("Consumo de Combustible")
+print("----------------------")
