c149844121485b96551423e59c6177f22fda4d67
Bicho Reactor <bichoreactor@gmail.com>
2024-10-17 17:14:39 -0300

    Clase 4: Mejoras al Ejercicio 2.

--- Clase 04/consumo_combustible.py
+++ Clase 04/consumo_combustible.py
@@ -8,12 +8,18 @@ Mostrar un detalle de los litros consumidos y el dinero gastado.
 
 print("Consumo de Combustible")
 print("----------------------")
-litros_por_100km = float(input("Ingresá la cantidad de litros que consume el coche por cada 100 km: "))
-costo_por_litro  = float(input("Ingresá el costo de cada litro de combustible: "))
-longitud_viaje   = float(input("Ingresá la longitud del viaje en kilómetros: "))
+while True:
+    litros_por_100km = float(input("Ingresá la cantidad de litros que consume el coche por cada 100 km: "))
+    costo_por_litro  = float(input("Ingresá el costo de cada litro de combustible: "))
+    longitud_viaje   = float(input("Ingresá la longitud del viaje en kilómetros: "))
 
-litros_consumidos = (litros_por_100km * longitud_viaje) / 100
-dinero_gastado    = litros_consumidos * costo_por_litro
+    litros_consumidos = (litros_por_100km * longitud_viaje) / 100
+    dinero_gastado    = litros_consumidos * costo_por_litro
 
-print("Litros de combustible consumidos: ", litros_consumidos)
-print("Dinero gastado en combustible: $", dinero_gastado)
+    print("Litros de combustible consumidos: ", litros_consumidos)
+    print("Dinero gastado en combustible: $", dinero_gastado)
+    
+    opcion = input("Para terminar ingrese una X ").upper()
+    
+    if opcion == "X":
+        break
