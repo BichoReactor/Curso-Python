8cd10771bbca0f927e4137c8cc5fea79398e310d
Bicho Reactor <musica.bichoreactor@gmail.com>
2024-12-03 22:35:44 -0300

    PFI: Diccionarios + Funciones.

--- Clase 12/Ejercicio2.py
+++ Clase 12/Ejercicio2.py
@@ -1,3 +1,12 @@
+"""
+Visualización personalizada de productos
+
+Agregá una función al sistema explicado en la clase, que permita simular la venta de un producto.
+El usuario o usuaria deberá ingresar el código del producto y la cantidad a vender.
+Si la cantidad en stock es suficiente, la función debe restar esa cantidad del inventario.
+Si la cantidad solicitada es mayor a la disponible, debe mostrar un mensaje de error.
+"""
+
 # Declaración de variables globales:
 productos = []
 
